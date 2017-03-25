52cbe0e756e9008d6c0194a7dd46218f66e0b96d
Andrea <andrea.93@hotmail.it>
2017-03-25 19:11:32 +0100

    added light and cube

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 var scene, camera, renderer;
+var cubes = [];
 
 init();
 
@@ -35,21 +36,68 @@ wall(4.3,2,0.3,4.3,1,10,0x003300);
 wall(8,2,0.3,1,1,8,0x003300);
 wall(0.3,2,3,4.85,1,6.5,0x003300);
 wall(0.3,2,3,-2.85,1,6.5,0x003300);
-wall(0.8,1,6.5,10.25,0.5,-8,0x003300);//garden right
-tree(10.25,1,-11)
-wall(0.8,1,6.5,10.25,0.5,-14.6,0x003300);//garden right
-tree(10.25,1,-18)
-wall(0.8,1,6.5,10.25,0.5,-21.2,0x003300);//garden right
-wall(0.8,1,6.5,-8.25,0.5,-8,0x003300);//garden left
-tree(-8.25,1,-11)
-wall(0.8,1,6.5,-8.25,0.5,-14.6,0x003300);//garden left
-tree(-8.25,1,-18)
-wall(0.8,1,6.5,-8.25,0.5,-21.2,0x003300);//garden left
-wall(5,1,0.8,-5.9,0.5,-24.6,0x003300);//garden center
+wall(0.8,1,6.5,10,0.5,-8.2,0x003300);//garden right
+tree(10,1,-11)
+wall(0.8,1,6.5,10,0.5,-14.7,0x003300);//garden right
+tree(10,1,-18)
+wall(0.8,1,6.5,10,0.5,-21.2,0x003300);//garden right
+wall(0.8,1,6.5,-8,0.5,-8,0x003300);//garden left
+tree(-7.99,1,-11)
+wall(0.8,1,6.5,-8,0.5,-14.5,0x003300);//garden left
+tree(-7.99,1,-18)
+wall(0.8,1,6.5,-8,0.5,-21.,0x003300);//garden left
+wall(6,1,0.8,-5.4,0.5,-24.6,0x003300);//garden center
 tree(-3.0,1,-24.6)
-wall(7,1,0.8,0.8,0.5,-24.6,0x003300);//garden center
+wall(7,1,0.8,0.9,0.5,-24.6,0x003300);//garden center
 tree(4.7,1,-24.6)
 wall(6,1,0.8,7.4,0.5,-24.6,0x003300);//garden center
+lightBulb(-6,1,-8,-7,3,-7);//left
+lightBulb(-6,1,-22,-7,3,-21);//left
+lightBulb(8,1,-8, 7.5, 3.1, -6.8);//right
+lightBulb(8,1,-22, 7, 3.1, -21);//right
+lamp(8,1.5,5, 8, 3.5, 6);//right
+lamp(-6,1.5,5,-7,3.5,6);//left
+lamp(1,1.5,15,0,3.5,16);//center
+moon();
+
+//Faccia inferiore
+
+cubes[0] = setCube(0.3,0.3,0.3,0.5,0.5,-20,0x6600CC);
+cubes[1] = setCube(0.3,0.3,0.3,1,0.5,-20,0x6600CC);
+cubes[2] = setCube(0.3,0.3,0.3,1.5,0.5,-20,0x6600CC);
+cubes[3] = setCube(0.3,0.3,0.3,0.5,0.5,-19.5,0x6600CC);
+cubes[4] = setCube(0.3,0.3,0.3,1,0.5,-19.5,0x6600CC);
+cubes[5] = setCube(0.3,0.3,0.3,1.5,0.5,-19.5,0x6600CC);
+cubes[6] = setCube(0.3,0.3,0.3,0.5,0.5,-19,0x6600CC);
+cubes[7] = setCube(0.3,0.3,0.3,1,0.5,-19,0x6600CC);
+cubes[8] = setCube(0.3,0.3,0.3,1.5,0.5,-19,0x6600CC);
+
+//Faccia centrale
+
+cubes[9] = setCube(0.3,0.3,0.3,0.5,1,-20,0x33FFCC);
+cubes[10] = setCube(0.3,0.3,0.3,1,1,-20,0x33FFCC);
+cubes[11] = setCube(0.3,0.3,0.3,1.5,1,-20,0x33FFCC);
+cubes[12] = setCube(0.3,0.3,0.3,0.5,1,-19.5,0x33FFCC);
+cubes[13] = setCube(0.3,0.3,0.3,1,1,-19.5,0x33FFCC);
+cubes[14] = setCube(0.3,0.3,0.3,1.5,1,-19.5,0x33FFCC);
+cubes[15] = setCube(0.3,0.3,0.3,0.5,1,-19,0x33FFCC);
+cubes[16] = setCube(0.3,0.3,0.3,1,1,-19,0x33FFCC);
+cubes[17] = setCube(0.3,0.3,0.3,1.5,1,-19,0x33FFCC);
+
+//Faccia superiore
+
+cubes[18] = setCube(0.3,0.3,0.3,0.5,1.5,-20,0x000099);
+cubes[19] = setCube(0.3,0.3,0.3,1,1.5,-20,0x000099);
+cubes[20] = setCube(0.3,0.3,0.3,1.5,1.5,-20,0x000099);
+cubes[21] = setCube(0.3,0.3,0.3,0.5,1.5,-19.5,0x000099);
+cubes[22] = setCube(0.3,0.3,0.3,1,1.5,-19.5,0x000099);
+cubes[23] = setCube(0.3,0.3,0.3,1.5,1.5,-19.5,0x000099);
+cubes[24] = setCube(0.3,0.3,0.3,0.5,1.5,-19,0x000099);
+cubes[25] = setCube(0.3,0.3,0.3,1,1.5,-19,0x000099);
+cubes[26] = setCube(0.3,0.3,0.3,1.5,1.5,-19,0x000099);
+
+
+
 
 star();
 animate();
@@ -68,6 +116,16 @@ function init() {
     renderer.setSize(WIDTH, HEIGHT);
     document.body.appendChild(renderer.domElement);
 
+    //Directional light
+
+        var light = new THREE.PointLight( 0xffffff, 1, 3000);
+        light.position.set( 50, 40, -100 );
+        scene.add( light );
+
+    
+    
+
+
     
 
     camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
@@ -78,7 +136,7 @@ function init() {
 
     camera.position.y = 0.3;
     camera.position.x = 1;
-    camera.position.z = 13;
+    camera.position.z = -15;
     
         
 
@@ -87,18 +145,18 @@ function init() {
     function plane (x, y, z){
             // plane
             var geometry = new THREE.PlaneGeometry( 400,400 );
-            var material = new THREE.MeshBasicMaterial( { color: 0x66CC33, side: THREE.DoubleSide } );
+            var material = new THREE.MeshLambertMaterial( { color: 0x66CC33, side: THREE.DoubleSide } );
             var ground = new THREE.Mesh( geometry, material );
 
             ground.position.y = -1.9; //lower it 
-			ground.rotation.x = -Math.PI/2; //-90 degrees around the xaxis 
+			      ground.rotation.x = -Math.PI/2; //-90 degrees around the xaxis 
 			//IMPORTANT, draw on both sides 
 
-			ground.position.x = x;
+			      ground.position.x = x;
             ground.position.y = y;
             ground.position.z = z;
 
-			scene.add(ground); 
+			      scene.add(ground); 
 
             
 
@@ -107,7 +165,7 @@ function init() {
   function wall(a,b,c,x,y,z,color){
 
 			var geometry = new THREE.BoxGeometry( a, b, c ); //a = width, b = height, c = depth
-			var material = new THREE.MeshBasicMaterial( { color: color } );
+			var material = new THREE.MeshLambertMaterial( { color: color } );
 			var wall = new THREE.Mesh( geometry, material );
 			scene.add( wall );
 
@@ -118,27 +176,107 @@ function init() {
             
         }
 
+function lightBulb(x,y,z,a,b,c){
+
+
+        cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.1, 0.1, 2, 100, 100, false), new THREE.MeshLambertMaterial({color: 0x000000}));
+		    scene.add(cylinder);
+
+        cylinder.position.x = x;
+        cylinder.position.y = y;
+        cylinder.position.z = z;
+
+        var geometry = new THREE.SphereGeometry(0.3,32,32);
+			  var material = new THREE.MeshLambertMaterial( { color: 0xFFFF99 } );
+        var sphere = new THREE.Mesh( geometry, material );
+        scene.add( sphere );
+
+        sphere.position.x = x;
+        sphere.position.y = 2;
+        sphere.position.z = z;
+
+        var light = new THREE.PointLight( 0xffffff, 1, 50, 1.5);
+        light.position.set( a, b, c );
+        scene.add( light );
+
+
+}
+
+function lamp(x,y,z,a,b,c){
+
+
+        cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.1, 0.1, 3, 100, 100, false), new THREE.MeshLambertMaterial({color: 0x000000}));
+		    scene.add(cylinder);
+
+        cylinder.position.x = x;
+        cylinder.position.y = y;
+        cylinder.position.z = z;
+
+        var geometry = new THREE.SphereGeometry(0.3,32,32);
+			  var material = new THREE.MeshLambertMaterial( { color: 0xFFFF99 } );
+        var sphere = new THREE.Mesh( geometry, material );
+        scene.add( sphere );
+
+        sphere.position.x = x;
+        sphere.position.y = 3;
+        sphere.position.z = z;
+
+        var light = new THREE.PointLight( 0xffffff, 1, 50, 1);
+        light.position.set( a, b, c );
+        scene.add( light );
+
+
+}
+
+function moon(){
+
+      var geometry = new THREE.SphereGeometry(10,32,32);
+			var material = new THREE.MeshBasicMaterial( { color: 0xFFFF99 } );
+      var moon = new THREE.Mesh( geometry, material );
+        moon.position.x = 50;
+        moon.position.y = 40;
+        moon.position.z = -100;
+        
+        scene.add(moon);
+
+}
+
+function setCube (a, b, c, x, y, z, color){
+      var geometry = new THREE.BoxGeometry( a, b, c );
+			var material = new THREE.MeshLambertMaterial( { color: color } );
+		  var cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+            cube.position.x = x;
+            cube.position.y = y;
+            cube.position.z = z;
+
+      return cube;
+
+
+  }
+
 		function column(x,y,z){
 
-		cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 2), new THREE.MeshBasicMaterial({color: 0x333333}));
+		cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 2), new THREE.MeshLambertMaterial({color: 0x333333}));
         
         
         cylinder.position.x = x;
         cylinder.position.y = y;
         cylinder.position.z = z;
-		scene.add(cylinder);
+		    scene.add(cylinder);
 
 }
 
 function tree(x,y,z){
 
-		cone = new THREE.Mesh(new THREE.CylinderGeometry(0, 1, 3, 50, 100, false), new THREE.MeshBasicMaterial({color: 0x003300}));
+		cone = new THREE.Mesh(new THREE.CylinderGeometry(0, 1, 3, 50, 100, false), new THREE.MeshLambertMaterial({color: 0x003300}));
         cone.position.x = x;
         cone.position.y = 3.5;
         cone.position.z = z;
 		scene.add(cone);
 
-    cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.4, 0.4, 2, 100, 100, false), new THREE.MeshBasicMaterial({color: 0x422515}));
+    cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.35, 0.35, 2, 100, 100, false), new THREE.MeshLambertMaterial({color: 0x422515}));
         cylinder.position.x = x;
         cylinder.position.y = y;
         cylinder.position.z = z;
@@ -166,6 +304,10 @@ function star(){
                 
 
              }
+
+
+
+
 
 
 
@@ -203,6 +345,11 @@ function star(){
  
     // Read more about requestAnimationFrame at http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
     requestAnimationFrame(animate);
+
+    cubes.forEach(function(cube) {
+      		cube.rotation.y += 0.05;
+          cube.rotation.x += 0.05;
+    });
  
     // Render the scene.
     renderer.render(scene, camera);
